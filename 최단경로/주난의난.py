a4cc0b66fe0115df4be74814387d00a24f7763f3
konghana <gksqlsl11@khu.ac.kr>
2021-10-06 00:07:55 +0900

    주난의 난(최단경로 / 백준 골드문제) 문제해결 완료

--- 최단경로/주난의난.py
+++ 최단경로/주난의난.py
@@ -1,11 +1,12 @@
 # https://www.acmicpc.net/problem/14497
 # 접근 방법
-# 
+# 현재 주난의 위치에서 상하좌우 방향으로 bfs방식으로 동작한다. 단, 1을 만나면 해당 위치의 스텝을 하나 증가시킨다.
+# 만약 #을 만날 경우 이를 출력한다.
 
 def dijkstra(x1, y1):
-    distance[x1-1][y1-1] = 0
+    distance[x1-1][y1-1] = 1
     q = []
-    heapq.heappush(q, [0, x1-1, y1-1])
+    heapq.heappush(q, [1, x1-1, y1-1])
     while q:
         count, x, y = heapq.heappop(q)
         if distance[x][y] < count:
