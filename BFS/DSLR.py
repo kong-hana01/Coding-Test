e6bc4c9edb2200ba3f95964f112e08bf1292bf2a
konghana <gksqlsl11@khu.ac.kr>
2021-10-10 02:40:46 +0900

    DSLR(bfs / 백준 골드문제) 문제 해결 완료

--- BFS/DSLR.py
+++ BFS/DSLR.py
@@ -13,13 +13,13 @@ for tc in range(int(input())):
         
         # D
         d = (2 * x) % 10000
-        if (not visited[d] or len(visited[d]) > len(visited[x]) + 1) and d != x:
+        if (not visited[d] or (len(visited[d]) > len(visited[x]) + 1)) and d != x:
             visited[d] = visited[x] + 'D'
             queue.append(d)
         
         # S
         s = x - 1 if x > 1 else 9999
-        if not visited[s] or len(visited[s]) > len(visited[x]) + 1:
+        if not visited[s] or (len(visited[s]) > len(visited[x]) + 1):
             visited[s] = visited[x] + 'S'
             queue.append(s)
 
@@ -28,7 +28,7 @@ for tc in range(int(input())):
         while len(l) < 4:
             l = '0' + l
         l = int(str(l)[1] + str(l)[2] + str(l)[3] + str(l)[0])
-        if (not visited[l] or len(visited[l]) > len(visited[x]) + 1) and l != x:
+        if (not visited[l] or (len(visited[l]) > len(visited[x]) + 1)) and l != x:
             visited[l] = visited[x] + 'L'
             queue.append(l)
         
@@ -37,7 +37,7 @@ for tc in range(int(input())):
         while len(r) < 4:
             r = '0' + r
         r = int(str(r)[3] + str(r)[0] + str(r)[1] + str(r)[2])
-        if (not visited[r] or len(visited[r]) > len(visited[x]) + 1) and r != x:
+        if (not visited[r] or (len(visited[r]) > len(visited[x]) + 1)) and r != x:
             visited[r] = visited[x] + 'R'
             queue.append(r)
         
