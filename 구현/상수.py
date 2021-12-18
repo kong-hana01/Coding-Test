4cddf4f928b20156c0d00fbe62f38d7ff0e8d1da
konghana <gksqlsl11@khu.ac.kr>
2021-12-19 03:09:45 +0900

    상수

---
+++ 구현/상수.py
@@ -0,0 +1,11 @@
+# https://www.acmicpc.net/problem/2908
+# 접근 방법
+# 세자리 수 중 거꾸로 한 수가 작은 수를 출력한다.
+
+a, b= input().split()
+print(a, b)
+a = a[::-1]
+b = b[::-1]
+print(a, b)
+
+print(int(a) if int(a) > int(b) else int(b))
