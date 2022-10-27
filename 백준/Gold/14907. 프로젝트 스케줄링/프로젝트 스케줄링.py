4ea3479d8db80499aa7e6fe61b1526e5ae1905ef
kong-hana01 <79015120+kong-hana01@users.noreply.github.com>
2022-10-27 14:56:50 +0900

    [Gold II] Title: 프로젝트 스케줄링, Time: 108 ms, Memory: 32492 KB -BaekjoonHub

--- 백준/Gold/14907. 프로젝트 스케줄링/프로젝트 스케줄링.py
+++ 백준/Gold/14907. 프로젝트 스케줄링/프로젝트 스케줄링.py
@@ -1,13 +1,9 @@
  # https://www.acmicpc.net/problem/14907
  # 접근방법
  # 위상정렬을 활용해 한 작업까지 오는 데의 걸리는 최대 시간을 계산하고, 이를 기준으로 프로젝트의 최소 시간을 계산한다.
-from re import L
 import sys
 from collections import deque
 lines = sys.stdin.readlines()
-# lines = []
-# for _ in range(6):
-#     lines.append(input())
 indegree = {chr(alpha): 0 for alpha in range(ord('A'), ord('Z')+1)}
 elapsed_time = {chr(alpha): 0 for alpha in range(ord('A'), ord('Z')+1)}
 work = {chr(alpha): 0 for alpha in range(ord('A'), ord('Z')+1)}
@@ -28,13 +24,11 @@ for line in lines:
         for next_alpha in indegree_alpha:
             graph[next_alpha].append(alpha)
 
-result = 0
 while queue:
     now = queue.popleft()
     for next in graph[now]:
         elapsed_time[next] = max(elapsed_time[next], elapsed_time[now]+work[next])
         indegree[next] -= 1
-        result = max(result, elapsed_time[next])
         if indegree[next] == 0:
             queue.append(next)
 
