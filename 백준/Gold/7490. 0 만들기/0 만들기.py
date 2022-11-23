5648ed7ac6552e87dff4d585f50f8d9f14026237
kong-hana01 <79015120+kong-hana01@users.noreply.github.com>
2022-11-23 19:33:37 +0900

    [Gold V] Title: 0 만들기, Time: 160 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Gold/7490. 0 만들기/0 만들기.py
+++ 백준/Gold/7490. 0 만들기/0 만들기.py
@@ -4,19 +4,15 @@
 def dfs(idx, now, n):
     if idx == n:
         if eval("".join(now.split())) == 0:
-            result.append(now)
+            print(now)
         return
+    dfs(idx+1, now+" "+arr[idx], n)    
     dfs(idx+1, now+"+"+arr[idx], n)
     dfs(idx+1, now+"-"+arr[idx], n)
-    dfs(idx+1, now+" "+arr[idx], n)    
 
 t = int(input())
 for _ in range(t):
     n = int(input())
     arr = [str(i) for i in range(1, n+1)]
-    result = []
     dfs(1, arr[0], n)
-    result.sort()
-    for x in result:
-        print(x)
     print()
